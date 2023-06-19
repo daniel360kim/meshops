3b073b0760141da9e29057b4bc342093ef524324
daniel360kim <nmrs.thrust@gmail.com>
2023-06-19 08:57:35 -0600

    made compatible with 3.10

--- python/meshthingy/media.py
+++ python/meshthingy/media.py
@@ -141,49 +141,50 @@ class NDSquareMesh:
     def get_iterable(self) -> torch.Tensor:
         return self.mesh
 
-    def run_timestep(self, conductivity_factor: float = 1) -> None:
-        # save current state before moving on
-        #self.history.append(self.mesh.clone())
-        
-        # Move tensors to GPU
-        
-        # Create tensors for averaging with shifted versions of the heatmap tensor
-        shifts = []
-        for i in range(self.dimensions):
-            shifts.append(torch.roll(self.mesh, shifts=1, dims=i))
-            shifts.append(torch.roll(self.mesh, shifts=-1, dims=i))
-        
-        # Set certain edges to zero so numbers don't wrap around
-        # TODO - context-aware padding,
-        # XXX OR JUST HANDLE THE EDGES WITH A SEPARATE CASE IN THE AVERAGING FUNCTION
-        #shifted_left[:, -1] = 0
-        #shifted_right[:, 0] = 0
-        #shifted_up[-1, :] = 0
-        #shifted_down[0, :] = 0
-        
-        # Apply padding for the edge cases
-        # TODO - context-aware padding
-        padded_shifts = []
-        _pad_dims = self._pad_dims(self.dimensions)
-        padded_mesh = torch.nn.functional.pad(self.mesh, _pad_dims, mode='constant', value=0)
-        
-        for tens in shifts:
-            # probably have a custom pad function that handles edges better, or just handle the edges in the averaging function
-            padded_shifts.append(torch.nn.functional.pad(tens, _pad_dims, mode='constant', value=1))
+def run_timestep(self, conductivity_factor: float = 1) -> None:
+    # save current state before moving on
+    # self.history.append(self.mesh.clone())
+    
+    # Move tensors to GPU
+    
+    # Create tensors for averaging with shifted versions of the heatmap tensor
+    shifts = []
+    for i in range(self.dimensions):
+        shifts.append(torch.roll(self.mesh, shifts=1, dims=i))
+        shifts.append(torch.roll(self.mesh, shifts=-1, dims=i))
+    
+    # Set certain edges to zero so numbers don't wrap around
+    # TODO - context-aware padding,
+    # XXX OR JUST HANDLE THE EDGES WITH A SEPARATE CASE IN THE AVERAGING FUNCTION
+    # shifted_left[:, -1] = 0
+    # shifted_right[:, 0] = 0
+    # shifted_up[-1, :] = 0
+    # shifted_down[0, :] = 0
+    
+    # Apply padding for the edge cases
+    # TODO - context-aware padding
+    padded_shifts = []
+    _pad_dims = self._pad_dims(self.dimensions)
+    padded_mesh = torch.nn.functional.pad(self.mesh, _pad_dims, mode='constant', value=0)
+    
+    for tens in shifts:
+        # probably have a custom pad function that handles edges better, or just handle the edges in the averaging function
+        padded_shifts.append(torch.nn.functional.pad(tens, _pad_dims, mode='constant', value=1))
 
-        # Calculate the average using tensors and avoid iteration
-        # Average the 3x3 square around each element
-        # TODO - add support for weighted averages
-        avg = (sum(padded_shifts)+padded_mesh) / (2 * self.dimensions + 1)
-        
-        # Remove padding
-        _remove_pad_slices = [slice(1,-1)]*self.dimensions
-        avg = avg[*_remove_pad_slices]
-        
-        # Move the result back to CPU if needed
-        avg = avg.cpu()
-        
-        self.mesh = avg
+    # Calculate the average using tensors and avoid iteration
+    # Average the 3x3 square around each element
+    # TODO - add support for weighted averages
+    avg = (sum(padded_shifts) + padded_mesh) / (2 * self.dimensions + 1)
+    
+    # Remove padding
+    _remove_pad_slices = [slice(1, -1)] * self.dimensions
+    avg = avg.__getitem__(_remove_pad_slices)
+    
+    # Move the result back to CPU if needed
+    avg = avg.cpu()
+    
+    self.mesh = avg
+
     
     def _get_mask(conductivity_factor: float = 1):
         """
