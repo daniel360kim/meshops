1d8159db9ac173e4ec901dedb392e418b4db00b8
cantthinkofausername16 <67755226+cantthinkofausername16@users.noreply.github.com>
2023-06-19 14:50:46 -0600

    optimize weighted pool

---
+++ python/tests/tests_padding.py
@@ -0,0 +1,43 @@
+from unittest import TestCase
+import torch
+from meshthingy.kinematics import apply_padding, apply_velocity
+
+# set device
+if torch.cuda.is_available():
+    device = torch.device('cuda')
+else:
+    device = torch.device('cpu')
+
+
+
+class PaddingTests(TestCase):
+        
+    def assertTensorsEqual(self, tensor1, tensor2):
+        self.assertTrue(torch.equal(tensor1, tensor2))
+    
+    def setUp(self):
+        self.tensor1 = torch.Tensor([
+            [1, 2],
+            [3, 4]
+        ])
+        super().setUp()
+    
+    def test_copy_padding(self):
+        target = torch.Tensor([
+            [1, 1, 2, 2],
+            [1, 1, 2, 2],
+            [3, 3, 4, 4],
+            [3, 3, 4, 4]
+        ])
+        self.assertTensorsEqual(target, apply_padding(self.tensor1, pad_type='copy'))
+    
+    def test_constant_padding(self):
+        target = torch.Tensor([
+            [0, 0, 0, 0],
+            [0, 1, 2, 0],
+            [0, 3, 4, 0],
+            [0, 0, 0, 0]
+        ])
+        self.assertTensorsEqual(target, apply_padding(self.tensor1, pad=0))
+        
+        
