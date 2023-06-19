ccf7dc15339b80474b06c6d1c573528441880de6
daniel360kim <nmrs.thrust@gmail.com>
2023-06-19 09:01:13 -0600

    removed errors

--- python/meshthingy/heatmap2d.py
+++ python/meshthingy/heatmap2d.py
@@ -8,11 +8,12 @@ import numpy as np
 from PIL import Image
 from debug_progress_bar import _get_progress_string
 import matplotlib.colors as colors
+from weighted_pool import weighted_pool
 
-torch.set_num_threads(6)
+#torch.set_num_threads(6)
 
 if torch.cuda.is_available():
-    print("CUDA is available, using GPU")
+    print("CUDA is available, using CUDA")
     device = torch.device("cuda")
 else:
     print("CUDA is not available, using CPU")
@@ -22,9 +23,11 @@ NUM_ITERATIONS = int(input("Iteration count: "))
 FPS = int(input("FPS: "))
 
 #constants
+#LENGTH = int(input("Length: "))
+#WIDTH = int(input("Width: "))
+#RADIUS = int(input("Radius of heating: "))
 LENGTH = 100
 WIDTH = 100
-RADIUS = 5
 
 gifFS = IterativeFile("images/2d/", "2D", ".gif")
 
@@ -35,44 +38,6 @@ logger = Log("logs/", LENGTH, WIDTH, NUM_ITERATIONS, FPS, gif_path)
 #Create average function Timer instance
 PERF_average_calc_timer = Timer()
 PERF_color_calc_timer = Timer()
-
-# convert to 1d tensor
-
-def runtimestep(heatmap: torch.Tensor, time_counter):
-    # Move tensors to GPU
-    heatmap = heatmap.to(device)
-    
-    # Create tensors for averaging with shifted versions of the heatmap tensor
-    shifted_left    = torch.roll(heatmap, shifts=1)
-    shifted_right   = torch.roll(heatmap, shifts=-1)
-    shifted_up      = torch.roll(heatmap, shifts=1, dims=0)
-    shifted_down    = torch.roll(heatmap, shifts=-1, dims=0)
-    
-    # Set certain edges to zero so numbers don't wrap around
-    shifted_left[:, -1] = 0
-    shifted_right[:, 0] = 0
-    shifted_up[-1, :] = 0
-    shifted_down[0, :] = 0
-    
-    # Apply padding for the edge cases
-    padded_heatmap       = torch.nn.functional.pad(heatmap, (1, 1, 1, 1), mode='constant', value=0)
-    padded_shifted_left  = torch.nn.functional.pad(shifted_left, (1, 1, 1, 1), mode='constant', value=0)
-    padded_shifted_right = torch.nn.functional.pad(shifted_right, (1, 1, 1, 1), mode='constant', value=0)
-    padded_shifted_up    = torch.nn.functional.pad(shifted_up, (1, 1, 1, 1), mode='constant', value=0)
-    padded_shifted_down  = torch.nn.functional.pad(shifted_down, (1, 1, 1, 1), mode='constant', value=0)
-    
-    # Calculate the average using tensors and avoid iteration
-    # Average the 3x3 square around each element
-    avg = (padded_heatmap + padded_shifted_left + padded_shifted_right + padded_shifted_up + padded_shifted_down) / 5
-    
-    # Remove padding
-    avg = avg[1:-1, 1:-1]
-    
-    # Move the result back to CPU if needed
-    avg = avg.cpu()
-    
-    return avg
-
 
 def get_rgb_ndarr(arr: torch.Tensor) -> np.ndarray:
     scaled_vals = -0.693 * arr + 0.693
@@ -87,7 +52,7 @@ def get_rgb_ndarr(arr: torch.Tensor) -> np.ndarray:
 time_counter = 0
 
 new_heatmap = NDSquareMesh((LENGTH, WIDTH), 0)
-new_heatmap.heat_region((50, 50), RADIUS+30, 1)
+new_heatmap.set_region((LENGTH//2, WIDTH//2), 10, 0.4)
 
 frames = []
 frames.append(Image.fromarray(get_rgb_ndarr(new_heatmap.get_iterable()), 'RGB'))
@@ -95,10 +60,10 @@ frames.append(Image.fromarray(get_rgb_ndarr(new_heatmap.get_iterable()), 'RGB'))
 for i in range(NUM_ITERATIONS):
     
     # constant heat source... add heat to the center of the mesh each iter
-    #new_heatmap.heat_region((50, 50), RADIUS, 1)
+    # new_heatmap.set_region((LENGTH//2, WIDTH//2), RADIUS, i/NUM_ITERATIONS)
     time_counter += 1
     
-    #new_heatmap.heat_region((50, 50), RADIUS, 0.4)
+    #new_heatmap.set_region((50, 50), RADIUS, 0.4)
 
     PERF_average_calc_timer.begin() # begin timer for average calculation
     new_heatmap.run_timestep()
@@ -111,7 +76,7 @@ for i in range(NUM_ITERATIONS):
     PERF_color_calc_timer.end() #end timer for color calculation
 
     logger.log(PERF_average_calc_timer, PERF_color_calc_timer, time_counter) # log the timings
-    print(f"{_get_progress_string(time_counter/NUM_ITERATIONS)} Iteration: {time_counter}/{NUM_ITERATIONS}", end="\r")
+    print(f"{_get_progress_string(time_counter/NUM_ITERATIONS)} Iteration: {time_counter}/{NUM_ITERATIONS}", end='\r')
     
 
 print(f"\n\nBest Averaging Time: {PERF_average_calc_timer.m_BestTime} us")
@@ -123,3 +88,38 @@ print(f"Worst Color Time: {PERF_color_calc_timer.m_WorstTime} us")
 print(f"Average Color Time: {round(PERF_color_calc_timer.m_AverageTime, 2)} us")
 
 draw_gif(frames, gif_path, FPS)
+
+#def runtimestep(heatmap: torch.Tensor, time_counter):
+#    # Move tensors to GPU
+#    heatmap = heatmap.to(device)
+#    
+#    # Create tensors for averaging with shifted versions of the heatmap tensor
+#    shifted_left    = torch.roll(heatmap, shifts=1)
+#    shifted_right   = torch.roll(heatmap, shifts=-1)
+#    shifted_up      = torch.roll(heatmap, shifts=1, dims=0)
+#    shifted_down    = torch.roll(heatmap, shifts=-1, dims=0)
+#    
+#    # Set certain edges to zero so numbers don't wrap around
+#    shifted_left[:, -1] = 0
+#    shifted_right[:, 0] = 0
+#    shifted_up[-1, :] = 0
+#    shifted_down[0, :] = 0
+#    
+#    # Apply padding for the edge cases
+#    padded_heatmap       = torch.nn.functional.pad(heatmap, (1, 1, 1, 1), mode='constant', value=0)
+#    padded_shifted_left  = torch.nn.functional.pad(shifted_left, (1, 1, 1, 1), mode='constant', value=0)
+#    padded_shifted_right = torch.nn.functional.pad(shifted_right, (1, 1, 1, 1), mode='constant', value=0)
+#    padded_shifted_up    = torch.nn.functional.pad(shifted_up, (1, 1, 1, 1), mode='constant', value=0)
+#    padded_shifted_down  = torch.nn.functional.pad(shifted_down, (1, 1, 1, 1), mode='constant', value=0)
+#    
+#    # Calculate the average using tensors and avoid iteration
+#    # Average the 3x3 square around each element
+#    avg = (padded_heatmap + padded_shifted_left + padded_shifted_right + padded_shifted_up + padded_shifted_down) / 5
+#    
+#    # Remove padding
+#    avg = avg[1:-1, 1:-1]
+#    
+#    # Move the result back to CPU if needed
+#    avg = avg.cpu()
+#    
+#    return avg
--- python/meshthingy/media.py
+++ python/meshthingy/media.py
@@ -1,11 +1,13 @@
 import torch
 import numpy as np
 from PIL import Image
+import itertools
 
 from log import Log
 from log import IterativeFile
 from timing import Timer
 from errors import UnsupportedDimensionError
+from weighted_pool import weighted_pool
 
 if torch.cuda.is_available():
     print("CUDA is available, using GPU")
@@ -50,7 +52,8 @@ class ConductiveBar:
         return self.mesh
     
 class ConductiveSurface:
-    def __init__(self, length: int, width: int, default_temp: float = 0):
+    def __init__(self, shape: tuple[int], default_temp: float = 0):
+        length, width = shape
         self.mesh = torch.Tensor(length, width).fill_(default_temp)
     
     def heat_square(self, loc: tuple = (0, 0), radius: int = 2, temperature: float = 0.5) -> None:
@@ -91,100 +94,118 @@ class NDSquareMesh:
         """
         # NDSquareMesh
         
-        Represents an n-dimensional square mesh, tensor, or array.
+        Represents an n-dimensional square mesh. Each cell can interact with its neighbors through
+        user defined interactions.
         
         ## Overloads
         `NDSquareMesh(pytorch_tensor: torch.Tensor)`
         `NDSquareMesh(shape: tuple[int], default_temp: int)`
         """
         
-        if len(args) == 1 and type(args[0]) == torch.Tensor:
-            self.mesh = args[0]
+        if len(args) == 1:
+            if type(args[0]) == torch.Tensor:
+                self.mesh = args[0]
+            elif type(args[0]) == np.ndarray:
+                self.mesh = torch.Tensor(args[0])
         
         elif len(args) == 2 and type(args[0] == tuple) and type(args[1] == int):
             # shape, default_temp
             shape, default_temp = args
-            print(shape, default_temp)
             self.mesh = torch.full(shape, default_temp)
-            print(self.mesh.shape)
             
         else:
             raise(TypeError("NDSquareMesh constructor accepts either `(tensor to convert): torch.Tensor` or `(shape): tuple[int], (default temp): int`"))
             
         self.shape = self.mesh.shape
         self.dimensions = len(self.shape)
-        self.history = []
+        #self.history = []
             
         self._pad_dims = lambda mask_radius: tuple([mask_radius] * 2 * self.dimensions)
     
-    def heat_region(self, center: tuple, radius: int = 2, temperature: float = 0.5) -> None:
+    def set_region(self, center: tuple, radius: int = 2, temperature: float = 0.5) -> None:
         """
-        Heats a "N-dimensional cube" of mesh around `center` to `temperature`.
+        Sets a "N-dimensional cube" of mesh around `center` to `temperature`.
         """
         
         # padded = torch.nn.functional.pad(self.mesh, self._pad_dims(1), mode='constant', value=0)
         
-        # Change the square region around loc to temperature
+        # change the square region around loc to temperature
         _nd_slice_obj = []
         
         for i in range(self.dimensions):
             _nd_slice_obj.append(slice(center[0] - radius, center[1] + radius + 1))
 
-        self.mesh.__setitem__(tuple(_nd_slice_obj), temperature)
-        
-        # Unpad
-        # print(f"before unpad: {self.mesh.shape}")
-        # _center_slice = [slice(radius, -radius)] * self.dimensions
-        # self.mesh = self.mesh.__getitem__(tuple(_center_slice))
-        # print(f"not sliced slice dims: {self.mesh.shape}")
+        self.mesh[*_nd_slice_obj] = temperature        
+         
+        # unpad
+        #print(f"before unpad: {self.mesh.shape}")
+        #_center_slice = [slice(radius,-radius)]*self.dimensions
+        #self.mesh = self.mesh[*_center_slice]
+        #print(f"not sliced slice dims: {self.mesh.shape}")
             
     def get_iterable(self) -> torch.Tensor:
         return self.mesh
 
-def run_timestep(self, conductivity_factor: float = 1) -> None:
-    # save current state before moving on
-    # self.history.append(self.mesh.clone())
-    
-    # Move tensors to GPU
-    
-    # Create tensors for averaging with shifted versions of the heatmap tensor
-    shifts = []
-    for i in range(self.dimensions):
-        shifts.append(torch.roll(self.mesh, shifts=1, dims=i))
-        shifts.append(torch.roll(self.mesh, shifts=-1, dims=i))
-    
-    # Set certain edges to zero so numbers don't wrap around
-    # TODO - context-aware padding,
-    # XXX OR JUST HANDLE THE EDGES WITH A SEPARATE CASE IN THE AVERAGING FUNCTION
-    # shifted_left[:, -1] = 0
-    # shifted_right[:, 0] = 0
-    # shifted_up[-1, :] = 0
-    # shifted_down[0, :] = 0
-    
-    # Apply padding for the edge cases
-    # TODO - context-aware padding
-    padded_shifts = []
-    _pad_dims = self._pad_dims(self.dimensions)
-    padded_mesh = torch.nn.functional.pad(self.mesh, _pad_dims, mode='constant', value=0)
-    
-    for tens in shifts:
-        # probably have a custom pad function that handles edges better, or just handle the edges in the averaging function
-        padded_shifts.append(torch.nn.functional.pad(tens, _pad_dims, mode='constant', value=1))
-
-    # Calculate the average using tensors and avoid iteration
-    # Average the 3x3 square around each element
-    # TODO - add support for weighted averages
-    avg = (sum(padded_shifts) + padded_mesh) / (2 * self.dimensions + 1)
-    
-    # Remove padding
-    _remove_pad_slices = [slice(1, -1)] * self.dimensions
-    avg = avg.__getitem__(_remove_pad_slices)
-    
-    # Move the result back to CPU if needed
-    avg = avg.cpu()
-    
-    self.mesh = avg
-
+    def run_timestep(
+        self, 
+        kernel: torch.Tensor | np.ndarray | list = [[0, 1, 0], [1, 1, 1], [0, 1, 0]], 
+        conductivity_factor: float = ...
+        ) -> None:
+        
+        if self.dimensions != 2:
+            raise NotImplementedError("Only 2D meshes are supported at the moment.")
+        
+        #interior_ranges = [
+        #    slice(kernel.shape[i]//2, -kernel.shape[i]//2) for i in range(self.dimensions)
+        #]
+        _ker_tensor = torch.Tensor(kernel)
+        # apply the kernel to each cell (& its neighbors) in the mesh
+        for coords in itertools.product(*[range(i) for i in self.shape]):
+            print(f"{coords[0]*100 + coords[1]}", end="\r")
+            self.mesh[*coords] = weighted_pool(
+                self.mesh,
+                coords[0],
+                coords[1],
+                _ker_tensor
+            )
+        
+        # Create tensors for averaging with shifted versions of the heatmap tensor
+        #shifts = []
+        #for i in range(self.dimensions):
+        #    shifts.append(torch.roll(self.mesh, shifts=1, dims=i))
+        #    shifts.append(torch.roll(self.mesh, shifts=-1, dims=i))
+        #
+        ## Set certain edges to zero so numbers don't wrap around
+        ## TODO - context-aware padding,
+        ## XXX OR JUST HANDLE THE EDGES WITH A SEPARATE CASE IN THE AVERAGING FUNCTION
+        ##shifted_left[:, -1] = 0
+        ##shifted_right[:, 0] = 0
+        ##shifted_up[-1, :] = 0
+        ##shifted_down[0, :] = 0
+        #
+        ## Apply padding for the edge cases
+        ## TODO - context-aware padding
+        #padded_shifts = []
+        #_pad_dims = self._pad_dims(1)
+        #padded_mesh = torch.nn.functional.pad(self.mesh, _pad_dims, mode='constant', value=0)
+        #
+        #for tens in shifts:
+        #    # probably have a custom pad function that handles edges better, or just handle the edges in the averaging function
+        #    padded_shifts.append(torch.nn.functional.pad(tens, _pad_dims, mode='constant', value=1))
+#
+        ## Calculate the average using tensors and avoid iteration
+        ## Average the 3x3 square around each element
+        ## TODO - add support for weighted averages
+        #avg = (sum(padded_shifts)+padded_mesh) / (2 * self.dimensions + 1)
+        #
+        ## Remove padding
+        #_remove_pad_slices = [slice(1,-1)]*self.dimensions
+        #avg = avg[*_remove_pad_slices]
+        #
+        ## Move the result back to CPU if needed
+        #avg = avg.cpu()
+        #
+        #self.mesh = avg
     
     def _get_mask(conductivity_factor: float = 1):
         """
